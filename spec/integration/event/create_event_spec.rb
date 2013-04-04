6fb24d2bfe8a82f0cafd134c7bf921f53b864628
Luiz Sanches <luizgrsanches@gmail.com>
2013-04-04 16:02:33 -0300

    ajustes na especificacao para passar nos testes

--- spec/integration/event/create_event_spec.rb
+++ spec/integration/event/create_event_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "Create event", :js => true do
+describe "Create event" do
   let!(:user) { create(:user, :paul) }
   let!(:other_user) { create(:user, :billy) }
   let!(:another_user) { create(:user, :luis) }
