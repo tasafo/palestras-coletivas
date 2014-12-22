5aaab44ee4cfad4287ff13641964a5d5acf1bc68
Luiz Sanches <luizgrsanches@gmail.com>
2014-12-22 18:44:51 -0300

    refatoracao da action show de eventos

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -41,56 +41,9 @@ class EventsController < ApplicationController
   end
 
   def show
-    unless @event.nil?
-      @dates = (@event.start_date..@event.end_date).to_a
-      @authorized = authorized_access?(@event)
+    @presenter = EventConcern.new(@event)
 
-      @open_enrollment = @event.deadline_date_enrollment >= Date.today
-
-      @can_record_presence = @authorized && Date.today >= @event.start_date
-
-      @show_users_present = Date.today > @event.end_date && !@can_record_presence
-
-      @users_present = []
-      @event.enrollments.presents.each { |e| @users_present << e.user }
-      @users_present.sort_by! { |u| u._slugs }
-
-      @users_active = []
-      @event.enrollments.actives.each { |e| @users_active << { :name => e.user._slugs, :enrollment => e } }
-      @users_active.sort_by! { |h| h[:name] }
-
-      @crowded = @users_active.count >= @event.stocking
-
-      @new_subscription = true
-
-      if logged_in?
-        @enrollment = Enrollment.where(:event => @event, :user => current_user).first
-
-        @new_subscription = false if @enrollment
-
-        @the_user_is_speaker = false
-
-        @event.schedules.each do |schedule|
-          if schedule.talk?
-            schedule.talk.users.each do |user|
-              @the_user_is_speaker = true if user.id == current_user.id
-            end
-          end
-        end
-
-        @open_enrollment = false if @the_user_is_speaker || @authorized
-      end
-
-      unless @event.to_public
-        @event = nil unless @authorized
-      end
-
-      @image_top = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'].sample
-
-      @can_vote = @event && @event.accepts_submissions && @event.end_date >= Date.today
-
-      render layout: 'event'
-    end
+    render layout: 'event'
   end
 
   def edit
@@ -129,8 +82,9 @@ class EventsController < ApplicationController
     end
 
     def event_params
-      params.require(:event).permit(:name, :edition, :description, :stocking, :tags, :start_date, :end_date, 
-        :deadline_date_enrollment, :accepts_submissions, :to_public, :place, :street, :district, :city, :state,
-        :country)
+      params.require(:event).permit(
+        :name, :edition, :description, :stocking, :tags, :start_date, :end_date, :deadline_date_enrollment, 
+        :accepts_submissions, :to_public, :place, :street, :district, :city, :state, :country
+      )
     end
 end
