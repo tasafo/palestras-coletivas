e247ca514edeba23287e4d2f5afa4be997877ce0
Luiz Sanches <luizgrsanches@gmail.com>
2015-05-14 00:08:32 -0300

    nao exibir eventos nao publicados na homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   layout 'home'
 
   def index
-    @events = Event.desc(:created_at).limit(3)
+    @events = Event.where(to_public: true).desc(:created_at).limit(3)
     @talkers = UserQuery.new.ranking(:presentation_events)
   end
 end
