b44b8c8a82d98be3d516e3483fd69a30bb3b7de2
Luiz Sanches <luizgrsanches@gmail.com>
2015-12-25 23:52:58 -0300

    update ruby, rails and airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_API_KEY']
+  config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  config.project_key = ENV['AIRBRAKE_API_KEY']
+  config.ignore_environments = %w(development test)
 end
