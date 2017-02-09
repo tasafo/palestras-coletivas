adcc8aba226e1ab47383161818d77ea6d2a93583
Luiz Sanches <luizsanches@opmbx.org>
2017-02-09 16:06:23 -0300

    fixes and improvements in the voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,36 +1,39 @@
 #:nodoc:
 class VotesController < ApplicationController
-  before_action :set_schedule, only: [:new, :create]
-
-  def new
-    if @event
-      @vote = Vote.create(schedule: @schedule, user: current_user)
-
-      @schedule.set_counter(:votes, :inc)
-    end
-
-    redirect_to _event_path, notice: t('flash.schedules.vote.add')
-  end
+  before_action :set_objects, only: [:create, :destroy]
 
   def create
-    if @event
-      @vote = Vote.find_by(schedule: @schedule, user: current_user)
-      @vote.destroy
-
-      @schedule.set_counter(:votes, :dec)
-
-      redirect_to _event_path, notice: t('flash.schedules.vote.remove')
+    unless @vote
+      @vote = current_user.votes.create(schedule: @schedule)
+      @schedule.set_counter(:votes, :inc)
+      @notice = 'add'
     end
+
+    redirect_to_event
+  end
+
+  def destroy
+    if @vote
+      @vote.destroy
+      @schedule.set_counter(:votes, :dec)
+      @notice = 'remove'
+    end
+
+    redirect_to_event
   end
 
   private
 
-  def set_schedule
+  def set_objects
     @event = Event.find(params.merge(only_path: true)[:event_id])
     @schedule = Schedule.find(params[:schedule_id])
+    @vote = current_user.votes.find_by(schedule: @schedule)
+    @notice = 'was_not_held'
+
+    redirect_to event_path(@event, anchor: 'schedule') and return unless @event
   end
 
-  def _event_path
-    "/events/#{@event._slugs[0]}#schedule"
+  def redirect_to_event
+    redirect_to event_path(@event, anchor: 'schedule'), notice: t("flash.schedules.vote.#{@notice}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Palestrascoletivas::Application.routes.draw do
 
   resources :events, except: :destroy do
     resources :schedules, only: [:new, :create, :edit, :update, :destroy] do
-      resources :votes, only: [:new, :create]
+      resources :votes, only: [:create, :destroy]
       resource :was_presented, only: [:create]
     end
     resources :enrollments, only: [:new, :create, :edit, :update], path: ":option_type"
