416e34b416939dd215c71f589b94df345b8afbe8
Luiz Sanches <luizsanches@opmbx.org>
2017-11-27 15:30:33 -0300

    travis ci error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,7 @@ RSpec.configure do |config|
   Capybara.javascript_driver = :webkit
 
   Capybara::Webkit.configure do |config|
-   config.block_unknown_urls
+    config.block_unknown_urls
   end
 
   config.before(:suite) do
