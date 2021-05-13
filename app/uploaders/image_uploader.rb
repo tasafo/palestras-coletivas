fc97bc8199f35ad11bd9f4fadd59b3d596cb10cd
Luiz Sanches <lsanches@protonmail.com>
2021-05-12 23:43:29 -0300

    Ajustes na imagem de capa do evento

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@
 class ImageUploader < Uploader
   if ENV['CLOUDINARY_URL'].present?
     cloudinary_transformation transformation: [
-      { width: 1024, height: 768, crop: :limit }
+      { width: 1920, height: 1080, crop: :limit }
     ]
   end
 end
