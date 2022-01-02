5d3dd7b68a1148ba7928954032e669310baca586
Luiz Sanches <lsanches@protonmail.com>
2022-01-02 08:24:53 -0300

    Index ignored

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,7 @@ class Vote
   include Mongoid::Timestamps::Created
 
   belongs_to :schedule, index: true
-  belongs_to :user, index: true
+  belongs_to :user
 
   after_create do
     schedule.inc(counter_votes: 1)
